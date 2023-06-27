b7c6c46875e6dd4bc3d76fa29e10cc2234883f7d
Michael Hu <luhuihu@outlook.com>
2023-06-26 23:46:39 -0700

    Add sa_target_single_mask in ummf
    sa_target_single_mask allows masking a desired object instead of all objects.
    
    Also, add some helper methods.
    
    Test: none

--- unified_multimodal.py
+++ unified_multimodal.py
@@ -8,6 +8,8 @@ from segment_anything import sam_model_registry, SamPredictor
 from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
 from super_gradients.training import models as yolon
 from typing import Any, Dict, List, Optional, Tuple
+import numpy as np
+import matplotlib.pyplot as plt
 import cv2
 
 
@@ -15,11 +17,11 @@ class UniMultiModalFramework:
 
     def __init__(
             self, 
-            sam_checkpoint, # "sam_vit_h_4b8939.pth"
-            sam_model_type, # "vit_h"
-            yolo_model, # "yolo_nas_l"
-            yolo_pretrained_weights, # "coco"
-            yolo_conf_threshold, # 0.25
+            sam_checkpoint: str, # "sam_vit_h_4b8939.pth"
+            sam_model_type: str, # "vit_h"
+            yolo_model: str, # "yolo_nas_l"
+            yolo_pretrained_weights: str, # "coco"
+            yolo_conf_threshold: int, # 0.25
             device: str = "cuda",
             name: str = 'ummf',
         ) -> None:
@@ -56,4 +58,74 @@ class UniMultiModalFramework:
             conf_threshold = self._conf_threshold
         detection = model.predict(image_path, conf=conf_threshold)
         return detection
+    
+    def sa_target_single_mask(
+            self, 
+            image_path: str,
+            objIndex: int = 0, # default the first object, it can be selected by using sa_yolo_detect together
+        ) -> List[Dict[str, Any]]:
+
+        detection_pred = self.sa_yolo_detect(image_path)._images_prediction_lst
+
+        bboxes_xyxy = detection_pred[objIndex].prediction.bboxes_xyxy.tolist()
+        confidence = detection_pred[objIndex].prediction.confidence.tolist()
+        labels = detection_pred[objIndex].prediction.labels.tolist()
+
+        image = cv2.imread(image_path)
+
+        predictor = SamPredictor(self._sam)
+        predictor.set_image(image)
+
+        image = image.transpose((2, 0, 1)) # Tranpose to match SAM input format
+        image = image / 255.0 # Normalize image values to [0, 1]
+        image = np.expand_dims(image, axis=0) # Add batch dimension
+
+        input_box = np.array(bboxes_xyxy[objIndex])
+
+        if labels[objIndex] == 0:
+            # predict masks using SAM
+            masks, _, _ = predictor.predict(
+                point_coords=None,
+                point_labels=None,
+                box=input_box[None, :],
+                multimask_output=False,
+            )
+        return masks # may return None
+
+    ## helpers
+    def show_mask(self, mask, ax, random_color=False):
+        if random_color:
+            color = np.concatenate([np.random.random(3), np.array([0.6])], aixs=0)
+        else:
+            color = np.array([30/255, 144/255, 255/255, 0.6])
+        h, w = mask.shape[-2:]
+        mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+        ax.imshow(mask_image)
+
+    def show_points(self, coords, labels, ax, marker_size=375):
+        pos_points = coords[labels==1]
+        neg_points = coords[labels==0]
+        ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=market_size, edgecolor='white', linewidth=1.25)
+        ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=market_size, edgecolor='white', linewidth=1.25)
+
+    def show_box(self, box, ax):
+        x0, y0 = box[0], box[1]
+        w, h = box[2] - box[0], box[3] - box[1]
+        ax.add_patch(plt.Rectangle(x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))
+
+    def show_anns(self, anns):
+        if len(anns) == 0:
+            return
+        sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+        ax = plt.gca()
+        ax.set_autoscale_on(False)
+        polygons = []
+        color = []
+        for ann in sorted_anns:
+            m = ann['segmentation']
+            img = np.ones(m.shape[0], m.shape[1], 3)
+            color_mask = np.random.random((1, 3)).tolist()[0]
+            for i in range(3):
+            img[:,:,i] = color_mask[i]
+            ax.imshow(np.dstack((img, m*0.35)))
 
